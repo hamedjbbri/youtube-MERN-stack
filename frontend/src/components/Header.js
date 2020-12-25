910242a7cf6411a6bf2d1d3425c6d8c7ab4cb026
Hamed Jabbari <jabbary.hamed@gmail.com>
2020-12-24 20:11:24 -0500

    Header added

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './Header.scss';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                 <div className="header__left">
+                   <img src="./assets/Logo/Logo-brainflix.png" alt="Logo" />
+                 </div>
+                 <div className="header__right">
+                   <form className="header__right--form">
+                      <input type="text" className="header__right--input"    
+                       placeholder="Search..."/>
+                       <button className="header__right--btn">
+                       <i className="fa fa-search header__right--search"></i> 
+                       </button>
+                    
+                   </form>
+
+                   <button className="header__right--button"  >+Upload</button>
+                 </div>
+            </div>
+        )
+    }
+}
